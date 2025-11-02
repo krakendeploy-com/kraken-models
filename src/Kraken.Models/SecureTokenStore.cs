68a659af631cbef3fb05375beed2e8f6921d1463
Klizzer <sebastian@lunartech.se>
2025-11-02 11:34:29 +0100

    Trigger build

--- src/Kraken.Models/SecureTokenStore.cs
+++ src/Kraken.Models/SecureTokenStore.cs
@@ -10,7 +10,6 @@ public static class SecureTokenStore
     {
         return Path.Combine(rootPath, "refresh.blob");
     }
-    
 
     public static void SaveRefreshToken(string platform, string rootPath, string refreshToken)
     {
